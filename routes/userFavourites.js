5329a9b8e572ac59e408cc21ad412530a33d6678
Anastasia <panoutsou.anastasia@gmail.com>
2022-02-07 10:17:35 +0000

    get all fetches only admin data in totem routes

--- routes/userFavourites.js
+++ routes/userFavourites.js
@@ -7,4 +7,13 @@ const { User } = require("../models/user");
 
 const { Sequelize } = require("sequelize");
 
+//=============== fetch all Totems via the Totem table============ //
+router.get("/:userid", async (req, res) => {
+  const favTotems = await UserFavourites.findall;
+  const totemsreturned = await Totem.findAll({
+    where: { createdBy: req.params.id },
+  });
+  res.status(200).json({ totemsreturned });
+});
+
 module.exports = router;
